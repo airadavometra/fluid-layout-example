cef5e87b24af2e15079b955dba34a9704b4e9d4e
Daria Artemova <airadavometra@gmail.com>
2024-02-15 17:21:56 +0100

    add routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./reset.css";
 import "./index.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>TODO: Comic page</div>,
+  },
+  {
+    path: "/about",
+    element: <div>TODO: About page</div>,
+  },
+  {
+    path: "/archive",
+    element: <div>TODO: Archive page</div>,
+  },
+  {
+    path: "/characters",
+    element: <div>TODO: Characters page</div>,
+  },
+  {
+    path: "/art",
+    element: <div>TODO: Art page</div>,
+  },
+  {
+    path: "/links",
+    element: <div>TODO: Links page</div>,
+  },
+  {
+    path: "*",
+    element: <div>TODO: 404 page</div>,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
